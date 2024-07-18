214f48335cfc3d4ef8dd4ddc593156ade05a3667
Reeba Patel <129683467+ReebaPatel@users.noreply.github.com>
2024-07-18 20:50:24 +0530

    [new commit]

--- script.js
+++ script.js
@@ -1,49 +1,50 @@
 document.addEventListener("DOMContentLoaded", () => {
-    loadComponents();
-  });
-  
-  function loadComponents() {
-    loadComponent("navbar", "navbar.html", "navbar.css");
-    loadComponent("aboutus", "aboutus.html", "aboutus.css");
-    loadComponent("counter-section", "counter.html", "counter.css");
-    loadComponent("initiatives", "initiatives.html", "initiatives.css");
-    loadComponent("sponsors", "sponsors.html", "sponsors.css");
-    loadComponent("testimonial", "testimonial.html", "testimonial.css");
-    loadComponent("footer", "footer.html", "footer.css");
-  }
-  
-  function loadComponent(id, file, cssFile) {
-    fetch(file)
-      .then((response) => response.text())
-      .then((data) => {
-        document.getElementById(id).innerHTML = data;
-        if (cssFile) {
-          loadCSS(cssFile);
-        }
-  
-        // Check if the loaded component is the testimonial and initialize the slider
-        if (id === "testimonial") {
-          initializeTestimonialSlider();
-        }
-      })
-      .catch((error) => console.error("Error loading component:", error));
-  }
-  
-  function loadCSS(cssFile) {
-    const link = document.createElement("link");
-    link.rel = "stylesheet";
-    link.href = cssFile;
-    document.head.appendChild(link);
-  }
-  
+  loadComponents();
+});
+
+function loadComponents() {
+  loadComponent("navbar", "navbar.html", "navbar.css");
+  loadComponent("hero-section", "hero-section.html", "hero-section.css");
+  loadComponent("aboutus", "aboutus.html", "aboutus.css");
+  loadComponent("counter-section", "counter.html", "counter.css");
+  loadComponent("initiatives", "initiatives.html", "initiatives.css");
+  loadComponent("sponsors", "sponsors.html", "sponsors.css");
+  loadComponent("testimonial", "testimonial.html", "testimonial.css");
+  loadComponent("footer", "footer.html", "footer.css");
+}
+
+function loadComponent(id, file, cssFile) {
+  fetch(file)
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById(id).innerHTML = data;
+      if (cssFile) {
+        loadCSS(cssFile);
+      }
+
+      // Check if the loaded component is the testimonial and initialize the slider
+      if (id === "testimonial") {
+        initializeTestimonialSlider();
+      }
+    })
+    .catch((error) => console.error("Error loading component:", error));
+}
+
+function loadCSS(cssFile) {
+  const link = document.createElement("link");
+  link.rel = "stylesheet";
+  link.href = cssFile;
+  document.head.appendChild(link);
+}
+
 function initializeTestimonialSlider() {
-    // Ensure jQuery is loaded
-    if (window.jQuery) {
-        $(document).ready(function () {
-            $.getJSON('speakers.json', function (data) {
-                var slidesHtml = '';
-                data.speakers.forEach(function (speaker) {
-                    slidesHtml += `
+  // Ensure jQuery is loaded
+  if (window.jQuery) {
+    $(document).ready(function () {
+      $.getJSON("speakers.json", function (data) {
+        var slidesHtml = "";
+        data.speakers.forEach(function (speaker) {
+          slidesHtml += `
                         <div class="slide">
                             <div class="sp-image">
                                 <img src="${speaker.image}">
@@ -55,95 +56,66 @@ function initializeTestimonialSlider() {
                                 </div>
                             </div>
                         </div>`;
-                });
-                $('#slider').html(slidesHtml);
-
-                // Initialize the slider
-                $('#slider').slick({
-                    autoplay: true,
-                    dots: true,
-                    infinite: true,
-                    pauseOnHover: false,
-                    centerMode: true,
-                    centerPadding: "18%"
-                });
-            });
         });
-    } else {
-        console.error('jQuery is not loaded.');
-    }
+        $("#slider").html(slidesHtml);
+
+        // Initialize the slider
+        $("#slider").slick({
+          autoplay: true,
+          dots: true,
+          infinite: true,
+          pauseOnHover: false,
+          centerMode: true,
+          centerPadding: "18%",
+        });
+      });
+    });
+  } else {
+    console.error("jQuery is not loaded.");
+  }
 }
-  document.addEventListener('DOMContentLoaded', function() {
-    // Popup Form 1
-    const openFormBtn1 = document.getElementById('openFormBtn1');
-    const popupForm1 = document.getElementById('popupForm1');
-    const closeBtn1 = popupForm1.querySelector('.closeBtn');
-  
-    openFormBtn1.addEventListener('click', function() {
-      popupForm1.style.display = 'block';
-    });
-  
-    closeBtn1.addEventListener('click', function() {
-      popupForm1.style.display = 'none';
-    });
-  
-    // Popup Form 2
-    const openFormBtn2 = document.getElementById('openFormBtn2');
-    const popupForm2 = document.getElementById('popupForm2');
-    const closeBtn2 = popupForm2.querySelector('.closeBtn');
-  
-    openFormBtn2.addEventListener('click', function() {
-      popupForm2.style.display = 'block';
-    });
-  
-    closeBtn2.addEventListener('click', function() {
-      popupForm2.style.display = 'none';
-    });
-  
-    // Popup Form 3
-    const openFormBtn3 = document.getElementById('openFormBtn3');
-    const popupForm3 = document.getElementById('popupForm3');
-    const closeBtn3 = popupForm3.querySelector('.closeBtn');
-  
-    openFormBtn3.addEventListener('click', function() {
-      popupForm3.style.display = 'block';
-    });
-  
-    closeBtn3.addEventListener('click', function() {
-      popupForm3.style.display = 'none';
-    });
-  });
-  
-  document.addEventListener('DOMContentLoaded', () => {
-    const container = document.getElementById('team-container');
 
-    fetch('team.json')
-        .then(response => response.json())
-        .then(data => {
-            data.forEach(member => {
-                const memberDiv = document.createElement('div');
-                memberDiv.classList.add('team-member');
 
-                const img = document.createElement('img');
-                img.src = member.image;
-                img.alt = `${member.name}'s photo`;
+document.addEventListener("DOMContentLoaded", () => {
+  const container = document.getElementById("team-container");
 
-                const name = document.createElement('h2');
-                name.textContent = member.name;
+  fetch("team.json")
+    .then((response) => response.json())
+    .then((data) => {
+      data.forEach((member) => {
+        const memberDiv = document.createElement("div");
+        memberDiv.classList.add("team-member");
 
-                const position = document.createElement('p');
-                position.textContent = member.position;
+        const img = document.createElement("img");
+        img.src = member.image;
+        img.alt = `${member.name}'s photo`;
 
-                const email = document.createElement('p');
-                email.textContent = member.email;
+        const name = document.createElement("h2");
+        name.textContent = member.name;
 
-                memberDiv.appendChild(img);
-                memberDiv.appendChild(name);
-                memberDiv.appendChild(position);
-                memberDiv.appendChild(email);
+        const position = document.createElement("p");
+        position.textContent = member.position;
 
-                container.appendChild(memberDiv);
-            });
-        })
-        .catch(error => console.error('Error loading team data:', error));
+        const email = document.createElement("p");
+        email.textContent = member.email;
+
+        memberDiv.appendChild(img);
+        memberDiv.appendChild(name);
+        memberDiv.appendChild(position);
+        memberDiv.appendChild(email);
+
+        container.appendChild(memberDiv);
+      });
+    })
+    .catch((error) => console.error("Error loading team data:", error));
 });
+$(document).ready(function () {
+  setInterval(function () {
+    $(".hr-style").toggleClass("hr-rotate");
+    $(".text-1-tleft").toggleClass("t1thide");
+    $(".text-1-bright").toggleClass("t1bhide");
+    $(".text-2-tright").toggleClass("t2tshow");
+    $(".text-2-bleft").toggleClass("t2bshow");
+  }, 2000);
+});
+
